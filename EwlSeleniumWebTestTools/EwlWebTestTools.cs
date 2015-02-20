4407ef73a2aa65f2f61faa60a2c7beb46636b058
Sam Rueby <samrueby@gmail.com>
2015-02-20 15:26:55 -0500

    Init

---
+++ EwlSeleniumWebTestTools/EwlWebTestTools.cs
@@ -0,0 +1,65 @@
+﻿using Humanizer;
+using OpenQA.Selenium;
+
+namespace SamRueby.EwlSeleniumWebTestTools {
+	public static class EwlWebTestTools {
+		public static void SelectOptionFromRadioButtonSelectList( this IWebDriver iWebDriver, string formItemLabel, string optionLabel ) {
+			iWebDriver.FindElement(
+				By.XPath(
+					( "//*[@class='formItemAndLabelGrouping']//*[contains(@class,'label')][contains(text(),'{0}')]" +
+					  "/ancestor::*[@class='formItemAndLabelGrouping']//label//span[contains(text(),'{1}')]//ancestor::label//input[@type='radio']" ).FormatWith(
+						  formItemLabel,
+						  optionLabel ) ) ).Click();
+		}
+
+		public static By GetTextBoxFromFormItem( string formItemLabel ) {
+			return
+				By.XPath(
+					"//*[@class='formItemAndLabelGrouping']//*[contains(@class,'label')][contains(text(),\"{0}\")]/ancestor::*[@class='formItemAndLabelGrouping']//input[@type='text']"
+						.FormatWith( formItemLabel ) );
+		}
+
+		public static By GetTextBoxFromLabel( string label ) {
+			return By.XPath( "//*[contains(@class,'ewfLabeledControl')][contains(text(),'{0}')]//input[@type='text']".FormatWith( label ) );
+		}
+
+		/// <summary>
+		/// <param name="divId"></param> should be the ID of the first div INSIDE (child) the div with the class of ewfDropDown. Include the hash.
+		/// </summary>
+		public static void ClickSelect2DropdownChooseOption( this IWebDriver driver, string divId, string optionChoiceText ) {
+			driver.FindElement( By.CssSelector( divId + " > a" ) ).Click();
+			driver.FindElement( By.XPath( "//ul[@class='select2-results']/li/div[contains(text(), '" + optionChoiceText + "')]" ) ).Click();
+		}
+
+		public static string GetFormItemTableValue( this IWebDriver driver, string leftColumnName ) {
+			return driver.FindElement( By.XPath( "//td[contains(text(), '" + leftColumnName + "')]/../td[2]" ) ).Text;
+		}
+
+		public static string GetTextBoxValue( this IWebDriver driver, By @by ) {
+			return driver.FindElement( @by ).GetAttribute( "value" );
+		}
+
+		public static bool RadioButtonIsChecked( this IWebDriver driver, By isEnabled ) {
+			return driver.FindElement( isEnabled ).GetAttribute( "checked" ) == "true";
+		}
+
+		public static By GetTableCellContaining( string text ) {
+			return By.XPath( "//td[contains(text(), '" + text + "')]" );
+		}
+
+		public static bool ElementExists( this IWebDriver driver, By by ) {
+			try {
+				driver.FindElement( @by );
+				return true;
+			}
+			catch( NoSuchElementException ) {
+				return false;
+			}
+		}
+
+		public static void ClickSelect2DropdownChooseOption( By select2, string optionChoiceText, IWebDriver driver ) {
+			driver.FindElement( select2 ).FindElement( By.ClassName( "select2-choice" ) ).Click();
+			driver.FindElement( By.XPath( "//ul[@class='select2-results']/li/div[contains(text(), '" + optionChoiceText + "')]" ) ).Click();
+		}
+	}
+}
---
+++ EwlSeleniumWebTestTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EwlSeleniumWebTestTools")]
+[assembly: AssemblyDescription("A class library for writing robust Selenium web tests with support for Enterprise Web Library. MIT License.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sam Rueby")]
+[assembly: AssemblyProduct("EwlSeleniumWebTestTools")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f79ffb1e-f96c-4522-b23f-ed6e2295aa2e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
